fee18fa7a34510e202b127873124de2bd7d85d68
Перевозчиков Михаил Антонович <124647024+GrinderQual@users.noreply.github.com>
2023-02-08 14:03:45 +0300

    Add files via upload

---
+++ Алгоритмы/Semester I/1-2/1_2.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	float c, a, b;
+	cin >> a >> b;
+	if (b == 0) {
+		cout << "#@$fb54@*^#ошибка!!! ";
+	}
+	else {
+		c = a / b;
+		cout << c;
+	}
+	return 0;
+}
